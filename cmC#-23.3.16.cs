71c7e56a841ebc4749c0ad8b0e94c5f2c2aa0abc
Catfan7 <111824521+Catfan7@users.noreply.github.com>
2023-03-16 16:20:25 -0700

    Update cmC#-23.3.16.cs
    Added boolean "newline" in the print statements that depicts whether or not to create a new line

--- cmC#-23.3.16.cs
+++ cmC#-23.3.16.cs
@@ -1,104 +1,212 @@
-#include <stdio.h>
+using System;
 
-// "newline" determines whether a new line is created after the statement is printed
-// set newline to 1 to do so
-void prts(char toPrint[], int newline) {
-    printf("%s", toPrint);
-    if (newline == 1) {
-        printf("\n");
+public class catmethods
+{
+    // "bool newline" determines whether a newline is created after the printed statement
+    public static void prts(String x, bool newline)
+    {
+        Console.Write(x);
+        if (newline)
+        {
+            Console.Write("\n");
+        }
     }
-}
-static void prti(int toPrint, int newline) {
-    printf("%d", toPrint);
-    if (newline == 1) {
-        printf("\n");
+    public static void prti(int x, bool newline) { 
+        Console.Write(x);
+        if (newline)
+        {
+            Console.Write("\n");
+        }
     }
-}
-static void prtf(float toPrint, int newline) {
-    printf("%f", toPrint);
-    if (newline == 1) {
-        printf("\n");
+    public static void prtf(float x, bool newline) { 
+        Console.Write(x);
+        if (newline)
+        {
+            Console.Write("\n");
+        }
     }
-}
-static void prtc(char toPrint, int newline) { 
-    printf("%c", toPrint);
-    if (newline == 1) {
-        printf("\n");
+    public static void prtd(double x, bool newline) { 
+        Console.Write(x);
+        if (newline)
+        {
+            Console.Write("\n");
+        }
     }
-}
-
-static double CelToFah(double x) {
-    return (x * 1.8) + 32;
-}
-static double CelToKel(double x) {
-    return x + 273.15;
-}
-static double CelToRan(double x) {
-    return (x * 1.8) + 491.67;
-}
-static double FahToCel(double x) {
-    return (x - 32) / 1.8;
-}
-static double FahToKel(double x) {
-    return FahToCel(x) + 273.15;
-}
-static double FahToRan(double x) {
-    return x + 459.67;
-}
-static double KelToCel(double x) {
-    return x - 273.15;
-}
-static double KelToFah(double x) {
-    return ((x - 273.15) * 1.8) + 32;
-}
-static double KelToRan(double x) {
-    return x * 1.8;
-}
-static double RanToCel(double x) {
-    return (x - 491.67) / 1.8;
-}
-static double RanToFah(double x) {
-    return x - 459.67;
-}
-static double RanToKel(double x) {
-    return x / 1.8;
-}
-static char * scanStr() {
-    char x[2056];
-    scanf("%s", x);
-    return x;
-}
-static int scanInt() {
-    int x;
-    scanf("%d", &x);
-    return x;
-}
-static float scanFlt() {
-    float x;
-    scanf("%f", x);
-    return x;
-}
-static void helloWorld() {
-    prts("Hello, world!", 1);
-}
-static int calculator(int op, int x, int y) {
-    int ans = 69420;
-    switch (op) {
-    case 0:
-        ans = x + y;
-        break;
-    case 1:
-        ans = x - y;
-        break;
-    case 2:
-        ans = x * y;
-        break;
-    case 3:
-        ans = x / y;
-        break;
-    case 4:
-        ans = x ^ y;
-        break;
+    public static void prtb(bool x, bool newline) { 
+        Console.Write(x);
+        if (newline)
+        {
+            Console.Write("\n");
+        }
     }
-    return ans;
+    public static void prtc(char x, bool newline) {
+        Console.Write(x);
+        if (newline)
+        {
+            Console.Write("\n");
+        }
+    }
+    public static double CelToFah(double x)
+    {
+        return (x * 1.8) + 32;
+    }
+    public static double CelToKel(double x)
+    {
+        return x + 273.15;
+    }
+    public static double CelToRan(double x)
+    {
+        return (x * 1.8) + 491.67;
+    }
+    public static double FahToCel(double x)
+    {
+        return (x - 32) / 1.8;
+    }
+    public static double FahToKel(double x)
+    {
+        return FahToCel(x) + 273.15;
+    }
+    public static double FahToRan(double x)
+    {
+        return x + 459.67;
+    }
+    public static double KelToCel(double x)
+    {
+        return x - 273.15;
+    }
+    public static double KelToFah(double x)
+    {
+        return ((x - 273.15) * 1.8) + 32;
+    }
+    public static double KelToRan(double x)
+    {
+        return x * 1.8;
+    }
+    public static double RanToCel(double x)
+    {
+        return (x - 491.67) / 1.8;
+    }
+    public static double RanToFah(double x)
+    {
+        return x - 459.67;
+    }
+    public static double RanToKel(double x)
+    {
+        return x / 1.8;
+    }
+    public static string scanStr()
+    {
+        return Console.ReadLine();
+    }
+    public static int scanInt()
+    {
+        return Convert.ToInt32(Console.ReadLine());
+    }
+    public static double scanDob()
+    {
+        return Convert.ToDouble(Console.ReadLine());
+    }
+    public static void helloWorld()
+    {
+        prts("Hello, world!", true);
+    }
+    public static int calculator(String op, int x, int y)
+    {
+        int ans = 32767;
+        switch (op)
+        {
+            case "add":
+                ans = x + y;
+                break;
+            case "subtract":
+                ans = x - y;
+                break;
+            case "multiply":
+                ans = x * y;
+                break;
+            case "divide":
+                ans = x / y;
+                break;
+            case "exponent":
+                ans = x ^ y;
+                break;
+        }
+        return ans;
+    }
+    public static double tempConverter(String inq, String outq, int input)
+    {
+        double output = 69.420;
+        switch (inq)
+        {
+            case "cel":
+                switch (outq)
+                {
+                    case "cel":
+                        output = input;
+                        break;
+                    case "fah":
+                        output = CelToFah(input);
+                        break;
+                    case "kel":
+                        output = CelToKel(input);
+                        break;
+                    case "ran":
+                        output = CelToRan(input);
+                        break;
+                }
+                break;
+            case "fah":
+                switch (outq)
+                {
+                    case "cel":
+                        output = FahToCel(input);
+                        break;
+                    case "fah":
+                        output = input;
+                        break;
+                    case "kel":
+                        output = FahToKel(input);
+                        break;
+                    case "ran":
+                        output = FahToRan(input);
+                        break;
+                }
+                break;
+            case "kel":
+                switch (outq)
+                {
+                    case "cel":
+                        output = KelToCel(input);
+                        break;
+                    case "fah":
+                        output = KelToFah(input);
+                        break;
+                    case "kel":
+                        output = input;
+                        break;
+                    case "ran":
+                        output = KelToRan(input);
+                        break;
+                }
+                break;
+            case "ran":
+                switch (outq)
+                {
+                    case "cel":
+                        output = RanToCel(input);
+                        break;
+                    case "fah":
+                        output = RanToFah(input);
+                        break;
+                    case "kel":
+                        output = RanToKel(input);
+                        break;
+                    case "ran":
+                        output = input;
+                        break;
+                }
+                break;
+        }
+        return output;
+    }
 }
